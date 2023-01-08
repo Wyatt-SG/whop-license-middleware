aa13dd811b5c46df33a010d4bfe3ed946d82013c
Wyatt Schulte <schultewya@gmail.com>
2023-01-07 19:43:46 -0500

    move head to document

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,17 @@
+import { NextPage } from "next";
 import Head from "next/head";
 
-export default function Home() {
+const Index: NextPage = () => {
   return (
     <>
       <Head>
         <title>DripAIO</title>
-        <meta
-          name="description"
-          content="Drip finds YOU the best ways of making $$$ online / Join today for free"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
         <div>hi 😉</div>
       </main>
     </>
   );
-}
+};
+
+export default Index;
