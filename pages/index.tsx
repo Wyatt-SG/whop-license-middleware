5aa9085df9e50c44151826d0d73750ea5b6adc76
Wyatt Schulte <schultewya@gmail.com>
2023-01-08 15:44:29 -0500

    fixed type issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import { createSwaggerSpec } from "next-swagger-doc";
 import dynamic from "next/dynamic";
+import Head from "next/head";
 import "swagger-ui-react/swagger-ui.css";
 
 const SwaggerUI = dynamic<{
   spec: any;
+  // @ts-ignore
 }>(import("swagger-ui-react"), { ssr: false });
 
 function ApiDoc({ spec }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <SwaggerUI spec={spec} />;
+  return (
+    <>
+      <Head>
+        <title>Whop License Middleware</title>
+      </Head>
+      <SwaggerUI spec={spec} />
+    </>
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
